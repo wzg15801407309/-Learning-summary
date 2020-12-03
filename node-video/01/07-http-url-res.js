458d6e58253791a49700a77f080715af65e60e2b
wzg15801407309 <1051084301@qq.com>
2020-12-03 17:59:14 +0800

    端口号

--- node-video/01/07-http-url-res.js
+++ node-video/01/07-http-url-res.js
@@ -6,6 +6,8 @@ server.on('request', function (req, res) {
   // console.log('客户端请求地址：', req.url);
   // // 可以直接使用end发送数据
   // res.end('hello node.js');
+  // 获取请求服务器的客户端的端口号
+  console.log('访问我的服务器的客户端的端口号：', req.socket.remotePort);
   var url = req.url;
   if (url === '/') {
     res.end('首页');
