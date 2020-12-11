5dd83a9ae520ac2caa809fa34ef3b06799d48fdd
zhuqin.wei <1051084301@qq.com>
2020-12-11 18:04:38 +0800

    20201211

--- node-video/02/feedback/app.js
+++ node-video/02/feedback/app.js
@@ -1,2 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+
+http.createServer(function (req,res){
+  var url = req.url;
+  if(url == '/' ){
+    fs.readFile('./views/index.html',function(err,data) {
+      if(err){
+        return res.end('404 Not Found.');
+      }
+      res.end(data);
+    });
+  }else if(url.indexOf('/public/') === 0){
+    fs.readFile('.'+'url',function(err,data){
+      return res.end('404 Not Found.');
+    });
+    res.end(data);
+  };
+}).listen(3000,function() {
+  console.log('Server is Running...');
+});
