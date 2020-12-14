fea8185c008b32ef1bd1a37bda9440d8990e3b51
zhuqin.wei <1051084301@qq.com>
2020-12-14 17:58:53 +0800

    20201214读取静态文件的bug

--- node-video/02/feedback/app.js
+++ node-video/02/feedback/app.js
@@ -7,15 +7,30 @@ http.createServer(function (req,res){
     fs.readFile('./views/index.html',function(err,data) {
       if(err){
         return res.end('404 Not Found.');
-      }
+      };
+      res.end(data);
+    });
+  }else if(url == '/post'){
+    fs.readFile('./views/post.html',function(err,data) {
+      if(err){
+        return res.end('404 Not Found.');
+      };
       res.end(data);
     });
   }else if(url.indexOf('/public/') === 0){
-    fs.readFile('.'+'url',function(err,data){
-      return res.end('404 Not Found.');
+    fs.readFile('.'+url,function(err,data){
+      if(err){
+        return res.end('404 Not Found.');
+      }
+      res.end(data.toString());
     });
-    res.end(data);
-  };
+  }else{
+    fs.readFile('./views/404.html',function(err,data) {
+      if(err){
+        return res.end('404 Not Found.');
+      };
+      res.end(data);
+  })};
 }).listen(3000,function() {
   console.log('Server is Running...');
 });
