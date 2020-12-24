f339d5d2598a8af601acc54601c1439cf78cc78f
zhuqin.wei <1051084301@qq.com>
2020-12-24 18:09:48 +0800

    20201224读取文件的路由、文件的封装

--- node-video/04/curd-express-01/app.js
+++ node-video/04/curd-express-01/app.js
@@ -1,24 +1,17 @@
-const { json } = require('express');
 const express = require('express');
-const fs = require('fs');
 const app = express();
+const router = require('./router');
+const bodyParser = require('body-parser');
 
 app.engine('html', require('express-art-template'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use('/node_modules/',express.static('../node_modules'));
 app.use('/public/',express.static('./public'));
 
-app.get('/',(req,res)=>{
-  fs.readFile('./db.josn','utf8',function(err,data){
-    if(err){
-      return res.status(500).send(" Server error.");
-    }
-    res.render('index.html',{
-      students: JSON.parse(data).students
-    });
-  })
-  
-});
-
+app.use(router);
 app.listen(3000,()=>{
   console.log(`Example app listening at http://127.0.0.1:3000`);
 })
---
+++ node-video/04/curd-express-01/fileoperations.js
@@ -0,0 +1,41 @@
+/**
+ * db.json 的操作
+ * 增、删、查、改
+ */
+const fs = require('fs');
+const dbpath = './db.josn';
+// 查
+exports.find = (callback)=>{
+  fs.readFile(dbpath,(err,data)=>{
+    if(err){
+      return callback(err);
+    }
+    callback(null,JSON.parse(data).students);
+  })
+};
+
+// 增
+exports.add = (obj,callback)=>{
+  fs.readFile(dbpath,(err,data)=>{
+    if(err){
+      return callback(err);
+    }
+    const data=JSON.parse(data).students;
+    obj.id=data[data.length-1].id+1;
+    const fileData=data.push(obj);
+    fs.writeFile(dbpath,JSON.stringify({
+      students: fileData
+    }),(err)=>{
+      if(err){
+        return callback(err);
+      }
+      callback(null);
+    });
+  })
+};
+
+// 删
+exports.delete = ()=>{};
+
+// 改/更新
+exports.update = ()=>{};
