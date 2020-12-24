31e7d53081fa91b36907a28aa8c30bd1046655ed
zhuqin.wei <1051084301@qq.com>
2020-12-24 15:18:14 +0800

    20201224fs

--- node-video/03/express/express-feedback/app.js
+++ node-video/03/express/express-feedback/app.js
@@ -1,5 +1,4 @@
-// const express = require('express');
-const { render } = require('art-template');
+
 const express = require('express');
 const bodyParser=require('body-parser');
 
--- node-video/04/curd-express/app.js
+++ node-video/04/curd-express/app.js
@@ -1,1 +1,24 @@
+const { json } = require('express');
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
+app.engine('html', require('express-art-template'));
+app.use('/node_modules/',express.static('../node_modules'));
+app.use('/public/',express.static('./public'));
+
+app.get('/',(req,res)=>{
+  fs.readFile('./db.josn','utf8',function(err,data){
+    if(err){
+      return res.status(500).send(" Server error.");
+    }
+    res.render('index.html',{
+      students: JSON.parse(data).students
+    });
+  })
+  
+});
+
+app.listen(3000,()=>{
+  console.log(`Example app listening at http://127.0.0.1:3000`);
+})
