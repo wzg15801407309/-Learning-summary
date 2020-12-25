7ec83d54511f2e062911ee5e736498e6197871a8
zhuqin.wei <1051084301@qq.com>
2020-12-25 11:40:46 +0800

    20201225add

--- node-video/04/curd-express-01/fileoperations.js
+++ node-video/04/curd-express-01/fileoperations.js
@@ -4,6 +4,7 @@
  */
 const fs = require('fs');
 const dbpath = './db.josn';
+
 // 查
 exports.find = (callback)=>{
   fs.readFile(dbpath,(err,data)=>{
@@ -20,18 +21,17 @@ exports.add = (obj,callback)=>{
     if(err){
       return callback(err);
     }
-    const data=JSON.parse(data).students;
-    obj.id=data[data.length-1].id+1;
-    const fileData=data.push(obj);
-    fs.writeFile(dbpath,JSON.stringify({
-      students: fileData
-    }),(err)=>{
+    const dataT=JSON.parse(data).students;
+    obj.id=dataT[dataT.length-1].id+1
+    const fileData=dataT.push(obj);
+
+    fs.writeFile(dbpath,JSON.stringify({students: dataT}),(err)=>{
       if(err){
         return callback(err);
       }
       callback(null);
     });
-  })
+  });
 };
 
 // 删
@@ -39,3 +39,8 @@ exports.delete = ()=>{};
 
 // 改/更新
 exports.update = ()=>{};
+
+
+
+
+
--- node-video/04/curd-express-01/router-old.js
+++ node-video/04/curd-express-01/router-old.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/',(req,res)=>{
   fileOperations.find((err,students)=>{
+    console.log('students: ', students);
     if(err){
       return res.status(500).send(" Server error.");
     }
@@ -18,13 +19,20 @@ router.get('/add',(req,res)=>{
   res.render('add.html');
 });
 router.post('/add',(req,res)=>{
-  console.log('req.body: ', req.body);
-  fileOperations.add(req.body,function(err){
-    if(err){
-      return res.status(500).send(" Server add error.");
-    }
-    res.redirect('/');
-  })
 
 });
 module.exports=router;
+// 这样也不好，express有路由
+// module.exports = (app)=>{
+//   app.get('/',(req,res)=>{
+//     fs.readFile('./db.josn','utf8',function(err,data){
+//       if(err){
+//         return res.status(500).send(" Server error.");
+//       }
+//       res.render('index.html',{
+//         students: JSON.parse(data).students
+//       });
+//     })
+    
+//   });
+// }
