7d4be7f380f90713104679e3e83eea0864316f5b
zhuqin.wei <1051084301@qq.com>
2020-12-25 17:56:59 +0800

    20201225实现文件的增删改查

--- node-video/04/curd-express-01/fileoperations.js
+++ node-video/04/curd-express-01/fileoperations.js
@@ -18,27 +18,62 @@ exports.find = (callback)=>{
 // 增
 exports.add = (obj,callback)=>{
   fs.readFile(dbpath,(err,data)=>{
-    if(err){
-      return callback(err);
-    }
+    if(err)return callback(err);
     const dataT=JSON.parse(data).students;
-    obj.id=dataT[dataT.length-1].id+1
-    const fileData=dataT.push(obj);
-
-    fs.writeFile(dbpath,JSON.stringify({students: dataT}),(err)=>{
-      if(err){
-        return callback(err);
-      }
-      callback(null);
-    });
+    obj.id=parseInt(dataT[dataT.length-1].id)+1
+    dataT.push(obj);
+    writeFile(dataT,callback);
   });
 };
 
-// 删
-exports.delete = ()=>{};
 
+//查找某一个数据
+exports.findById = (id,callback)=>{
+  fs.readFile(dbpath,'utf8',(err,data)=>{
+    if(err)return callback(err);
+    const students=JSON.parse(data).students
+    var stu= students.find((item)=>{
+      return item.id == id
+    });
+    callback(null,stu);
+  });
+};
 // 改/更新
-exports.update = ()=>{};
+exports.update = (obj,callback)=>{
+  fs.readFile(dbpath,'utf8',(err,data)=>{
+    if(err)return callback(err);
+    const students = JSON.parse(data).students
+    const stu = students.find((item)=>{
+      return item.id == obj.id
+    });
+    for (var key in obj) {
+      stu[key] = obj[key]
+    }
+    writeFile(students,callback);
+  });
+};
+// 删
+exports.delete = (id,callback)=>{
+  fs.readFile(dbpath,'utf8',(err,data)=>{
+    if(err)return callback(err);
+    const students =JSON.parse(data).students
+    const  index = students.findIndex((item)=>{
+      return item.id == id
+    });
+    students.splice(index,1);
+    writeFile(students,callback);
+  });
+};
+
+// 写文件保存数据
+const writeFile = (value,callback)=>{
+  fs.writeFile(dbpath,JSON.stringify({students: value}),(err)=>{
+    if(err){
+      return callback(err);
+    }
+    callback(null);
+  });
+}
 
 
 
--- node-video/04/curd-express-01/router.js
+++ node-video/04/curd-express-01/router.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require('express');
 const fileOperations = require('./fileoperations');
 
@@ -18,7 +17,6 @@ router.get('/add',(req,res)=>{
   res.render('add.html');
 });
 router.post('/add',(req,res)=>{
-  console.log('req.body: ', req.body);
   fileOperations.add(req.body,function(err){
     if(err){
       return res.status(500).send(" Server add error.");
@@ -27,4 +25,27 @@ router.post('/add',(req,res)=>{
   })
 
 });
+router.get('/edit',(req,res)=>{
+  fileOperations.findById(parseInt(req.query.id),(err,students)=>{
+    if(err) return res.status(500).send(" Server error.");
+    res.render('edit.html',{
+      students:students
+    })
+  });
+});
+router.post('/edit',(req,res)=>{
+  fileOperations.update(req.body,function(err){
+    if(err){
+      return res.status(500).send(" Server add error.");
+    }
+    res.redirect('/');
+  })
+});
+
+router.get('/delete',(req,res)=>{
+  fileOperations.delete(parseInt(req.query.id),(err)=>{
+    if(err) return res.status(500).send(" Server error.");
+    res.redirect('/');
+  });
+})
 module.exports=router;
