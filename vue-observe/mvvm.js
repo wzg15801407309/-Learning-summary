c3034c6b9f10882cad835b4857adcd8dc0ab20d4
zhuqin.wei <zhuqin.wei@zhuqinweideMacBook-Pro.local>
2021-03-25 09:15:20 +0800

    20210325

--- vue-observe/mvvm.js
+++ vue-observe/mvvm.js
@@ -29,7 +29,7 @@ function observe(data){
                     return
                 }
                 val=newvalue;
-                Observe(data);
+                Observe(val);
             }
         });
     }
