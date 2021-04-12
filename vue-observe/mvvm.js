c948de3f55c992603477b11ce00abba10bced0f5
zhuqin.wei <zhuqin.wei@zhuqinweideMacBook-Pro.local>
2021-04-12 18:46:01 +0800

    我也不知道改了什么

--- vue-observe/mvvm.js
+++ vue-observe/mvvm.js
@@ -1,7 +1,7 @@
 function WZG(option){
     this.$options=option;
     var data=this._data=this.$options.data;
-    // 数据劫持
+    // 数据劫持 设置get和set
     Observe(data);
     for (let key in data) {
         Object.defineProperty(this,key,{
@@ -13,8 +13,11 @@ function WZG(option){
                 this._data[key] = newvalue;
             }
         })
-    }
+    };
+    // 编译模版
+    new Compile(option.el,this);
 };
+// 数据劫持 实现方法
 function observe(data){
     for(let key in data){
         let val =data[key];
@@ -35,5 +38,33 @@ function observe(data){
     }
 };
 function Observe(data){
+    if(typeof data != 'object') return;
     return new observe(data);
+}
+// 编译模版
+function Compile(el,vm){
+
+    // el 是替换的范围 
+    vm.$el=document.querySelector(el);
+    console.log(vm.$el);
+    // 获取到需要到内存 为了减少dom的操作
+    let fragment=document.createDocumentFragment();
+    while(child = vm.$el.firstChild){
+        fragment.appendChild(child);
+    }
+    replace(fragment);
+    function replace(fragment){
+        Array.from(fragment.childNodes).forEach(function(node){
+            let text=node.textContent;
+            let reg=/\{\{(.*)\}\}/;
+            if(node.nodeType === 3 && reg.test(text)){
+                console.log(RegExp.$1);
+            }
+            if(node.childNodes){
+                replace(node);
+            }
+        })
+    }
+  
+    vm.$el.appendChild(fragment);
 }
