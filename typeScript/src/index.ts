5f43e86fcc662d30e8c4d9c7f6b083f2c7202be2
wzg15801407309 <1051084301@qq.com>
2022-07-28 17:02:37 +0800

    type

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -6,3 +6,11 @@ class Test {
 
 let t: Test = new Test();
 t.sayHello();
+//  如果变量的声明和赋值是同事进行的，TS可以自动对变量进行类型检测
+let a = true;
+// string[]==Array<number> 字符串数组  
+// 元祖 是固定长度的数组
+let h:[string,number] = ['1',2];
+// 类型别名
+
+type mytype = string;
