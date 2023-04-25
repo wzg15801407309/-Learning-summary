3824a3d922b0b8bc6297cb6970e51b02f8e29cd2
wzg15801407309 <1051084301@qq.com>
2023-04-25 15:52:52 +0800

    20230425

--- ts/ts_snake/src/moudls/Snake.ts
+++ ts/ts_snake/src/moudls/Snake.ts
@@ -26,7 +26,16 @@ class Snake{
     if(value<0 || value >290){
       throw new Error("蛇撞墙了！");
     }
-     this.head.style.left = value+"px";
+
+    if(this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value){// 想要调头
+      if(value > this.X){//说明
+        value = this.X-10;
+      }else{
+        value = this.X+10;
+      }
+    }
+    this.moveBody();
+    this.head.style.left = value+"px";
   }
   set Y(value:number){
     if(this.Y === value){
@@ -35,10 +44,39 @@ class Snake{
     if(value<0 || value >290){
       throw new Error("蛇撞墙了！");
     }
+    if(this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value){// 想要调头
+      if(value > this.Y){
+        value = this.Y-10;
+      }else{
+        value = this.Y+10;
+      }
+    }
+    this.moveBody();
     this.head.style.top = value+"px";
   }
   addBody(){
     this.element.insertAdjacentHTML("beforeend","<div></div>");
+  };
+  moveBody(){
+    for (let i = this.bodies.length-1; i >0; i--) {
+      let X = (this.bodies[i-1] as HTMLElement).offsetLeft;
+      let Y = (this.bodies[i-1] as HTMLElement).offsetTop;
+
+      (this.bodies[i] as HTMLElement).style.left = X +"px";
+      (this.bodies[i] as HTMLElement).style.top = Y +"px";
+
+    }
+  };
+  checkHeadBody(){
+    for (let i = 0; i < this.bodies.length; i++) {
+      let bd = this.bodies[1] as HTMLElement;
+      if(this.X === bd.offsetLeft && this.Y === bd.offsetTop){// 头和身体相撞了
+
+        throw new Error("撞到了自己～～！游戏GAME OVER!");
+
+      }
+      
+    }
   }
 }
 
